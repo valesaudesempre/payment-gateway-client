942d954bf2e393a4be4053d4c1f390498786d125
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2022-11-21 11:10:58 -0300

    feat(gateways): add criação de método de pgto. em IuguGateway

--- src/Gateways/Contracts/GatewayInterface.php
+++ src/Gateways/Contracts/GatewayInterface.php
@@ -3,6 +3,8 @@
 namespace ValeSaude\PaymentGatewayClient\Gateways\Contracts;
 
 use ValeSaude\PaymentGatewayClient\Customer\CustomerDTO;
+use ValeSaude\PaymentGatewayClient\Customer\GatewayPaymentMethodDTO;
+use ValeSaude\PaymentGatewayClient\Customer\PaymentMethodDTO;
 use ValeSaude\PaymentGatewayClient\Gateways\Enums\GatewayFeature;
 
 interface GatewayInterface
@@ -10,6 +12,12 @@ interface GatewayInterface
     public function createCustomer(CustomerDTO $data, string $externalReference): string;
 
     public function updateCustomer(string $id, CustomerDTO $data): void;
+
+    public function createPaymentMethod(
+        string $customerId,
+        PaymentMethodDTO $data,
+        bool $setAsDefault = true
+    ): GatewayPaymentMethodDTO;
 
     public function getGatewayIdentifier(): string;
 
