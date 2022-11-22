d1beac5b51d43fa673dd22a41da0ec2d2af635f5
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2022-11-22 18:05:22 -0300

    test(gateway): add testes para criação de fatura via IuguGateway

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace ValeSaude\PaymentGatewayClient\Tests;
 
+use Faker\Provider\pt_BR\Company;
 use Faker\Provider\pt_BR\Person;
 use Illuminate\Foundation\Testing\WithFaker;
 use Orchestra\Testbench\TestCase as BaseTestCase;
@@ -16,6 +17,7 @@ class TestCase extends BaseTestCase
         parent::setUp();
 
         $this->faker->addProvider(new Person($this->faker));
+        $this->faker->addProvider(new Company($this->faker));
     }
 
     protected function getPackageProviders($app): array
