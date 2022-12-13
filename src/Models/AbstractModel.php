a8bec8c71130b67ca4dafbf8bb36d48cb0a41d80
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2022-12-13 13:31:06 -0300

    feat(models): add ordenação padrão a todos os models

--- src/Models/AbstractModel.php
+++ src/Models/AbstractModel.php
@@ -3,12 +3,15 @@
 namespace ValeSaude\PaymentGatewayClient\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use ValeSaude\PaymentGatewayClient\Models\Concerns\HasDefaultOrderingTrait;
 
 /**
  * @property string $id
  */
 abstract class AbstractModel extends Model
 {
+    use HasDefaultOrderingTrait;
+
     public $incrementing = false;
     protected $guarded = [];
     protected $keyType = 'string';
---
+++ src/Models/Concerns/HasDefaultOrderingTrait.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ValeSaude\PaymentGatewayClient\Models\Concerns;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @method static Builder withoutDefaultOrdering()
+ *
+ * @mixin Model
+ */
+trait HasDefaultOrderingTrait
+{
+    public function scopeWithoutDefaultOrdering(Builder $query): Builder
+    {
+        return $query->withoutGlobalScope('latest');
+    }
+
+    public static function bootHasDefaultOrderingTrait(): void
+    {
+        static::addGlobalScope('latest', static function (Builder $query) {
+            $query->latest();
+        });
+    }
+}
